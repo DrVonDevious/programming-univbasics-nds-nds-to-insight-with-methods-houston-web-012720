caa19e81a2171a7fb91b2aa46bb453611a971a14
William C Rutland <william.prometheus@gmail.com>
2020-01-03 04:44:57 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,8 +4,11 @@ require 'directors_database'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 
+pp directors_database
+
 def gross_for_director(director_data)
-  gross = 
+  gross = 0
+  for i in dir
 end
 
 # Write a method that, given an NDS creates a new Hash
