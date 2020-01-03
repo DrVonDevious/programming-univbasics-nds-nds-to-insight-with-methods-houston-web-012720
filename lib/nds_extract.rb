536a1dfbc111f222aa9998e72b39f898780f19d3
William C Rutland <william.prometheus@gmail.com>
2020-01-03 04:50:40 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,8 +4,6 @@ require 'directors_database'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 
-pp directors_database
-
 def gross_for_director(director_data)
   p director_data
 end
